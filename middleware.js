1ce390aefd40eaa808887f977b8bf066ff2fc926
ndadevdev <nnandaa21.12@gmail.com>
2026-07-29 16:35:00 +0700

    Add Edge Middleware for mobile detection on Vercel

---
+++ middleware.js
@@ -0,0 +1,10 @@
+export default function middleware(req) {
+  const url = new URL(req.url);
+  if (url.pathname === '/') {
+    const ua = req.headers.get('user-agent') || '';
+    if (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini|mobile/i.test(ua)) {
+      url.pathname = '/indexm.html';
+      return Response.redirect(url, 302);
+    }
+  }
+}
